2d8a049c2af7a2f18acc737e00faf2d1e2ecd6e9
Mert Yildirim <merts.software@gmail.com>
2025-05-04 02:24:53 +0300

    🔥 Build hatası düzeltildi - carriers.js import path yapildisonins

--- pages/api/portal/carriers.js
+++ pages/api/portal/carriers.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../api/portal/[...nextauth]';
+import { authOptions } from './[...nextauth]';
 import { connectToDatabase } from '../../../lib/minimal-mongodb';
 
 export default async function handler(req, res) {
