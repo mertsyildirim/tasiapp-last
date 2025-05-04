88c8ba4f2ccd77cf07c7c3cffe86540dcf7400a2
Mert Yildirim <merts.software@gmail.com>
2025-05-04 19:45:38 +0300

    apple

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="tr">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body className="bg-gray-50">
